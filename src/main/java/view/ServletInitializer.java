a777cd66ff3fdc9ac4128ce92318a73e3c6d45d0
samarjit samanta <samarjit.samanta@gmail.com>
2014-11-18 02:21:33 +0800

    test

--- src/main/java/view/ServletInitializer.java
+++ src/main/java/view/ServletInitializer.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.lightadmin.api.config.LightAdmin;
+import org.lightadmin.core.util.LightAdminConfigurationUtils;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
@@ -25,7 +26,10 @@ public class ServletInitializer extends SpringBootServletInitializer {
                 .baseUrl("/admin")
                 .security(false)
                 .backToSiteUrl("http://localhost:8080");
- 
+        servletContext.setInitParameter(LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BASE_URL, "/admin");
+        servletContext.setInitParameter(LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BACK_TO_SITE_URL, "http://localhost:8080");
+        servletContext.setInitParameter(LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BASE_PACKAGE, "hello");
+
         super.onStartup(servletContext);
     }
 	
